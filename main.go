c49975f0f1635cc3b9be5470a74ec140e710bf31
arsenabdigali <abdigal.2015@gmail.com>
2026-02-12 22:25:54 +0500

    use chi

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -10,8 +12,10 @@ func main() {
 		Timeout: 10 * time.Second,
 	})
 
-	http.Handle("/proxy/", proxy)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	router := chi.NewRouter()
+	router.Handle("/proxy/*", proxy)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		return
 	}
 }
